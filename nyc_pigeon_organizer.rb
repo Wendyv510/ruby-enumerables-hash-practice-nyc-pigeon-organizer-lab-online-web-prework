f8f783fe11d3171d311ec54c9632a57ade53b24b
Wendy Harris <wendyv510@hotmail.com>
2019-09-24 16:51:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_date-{ 
+    :color =>{
+      :purple =>
+     ["Theo", "peter Jr.", "Lucky"],
+      :grey => ["Theo", "Peter Jr.", "Ms. K"],
+      :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+      :brown => ["Queenie", "Alex"]
+    }, 
+    :gender => {
+      :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+      :female => [ "Queenie", "Ms. K"]
+    },
+    :lives => {
+      "Subway" => ["Alex", "Ms. K", "Lucky"],
+      "Library" => ["Peter Jr."],
+      "City Hall" => ["Andrew"]
+    }
+  }
+    
 end
