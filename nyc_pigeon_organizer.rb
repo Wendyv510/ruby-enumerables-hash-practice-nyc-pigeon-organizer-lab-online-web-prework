b329bb80331ebf5bc26830107818edccfdc5f46f
Wendy Harris <wendyv510@hotmail.com>
2019-09-24 17:03:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_date-{ 
+    :color =>{
+      :purple =>
+     ["Theo", "peter Jr.", "Lucky"],
+      :grey => ["Theo", "Peter Jr.", "Ms. K"],
+      :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+      :brown => ["Queenie", "Alex"]
+    }, 
+    :gender => {
+      :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+      :female => [ "Queenie", "Ms. K"]
+    },
+    :lives => {
+      "Subway" => ["Alex", "Ms. K", "Lucky"],
+      "Library" => ["Peter Jr."],
+      "City Hall" => ["Andrew"]
+    }
+  }
+    
 end
+
+
+do 
+  .map{"Theo" = [:color, :gender, :lives]}
+  {"Peter Jr." = [:color, :gender, :lives]}
+  {"Lucky" = [:color, :gender, :lives]}
+  {"Ms. K" = [:color, :gender, :lives]}
+  {"Queenie" = [:color, :gender, :lives]}
+  {"Andrew" = [:color, :gender, :lives]}
+  {"Alex" = [:color, :gender, :lives]}
+  
+end
+
+  do
+    .map {{:name} = [:color, :gender, :lives]}
+  end 
+  
+  
+  
